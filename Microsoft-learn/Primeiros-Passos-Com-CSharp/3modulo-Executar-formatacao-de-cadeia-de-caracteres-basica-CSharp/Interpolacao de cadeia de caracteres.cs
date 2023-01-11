3f9dc9c667d08f353814d88804334e291929522d
Igor Fernandes Vital <igorflyv@gmail.com>
2023-01-11 17:10:44 -0300

    Etapa 1 - Usar a interpolacao de cadeia de caracteres para combinar uma cadeia de caracteres literal e um valor variavel

---
+++ Microsoft-learn/Primeiros-Passos-Com-CSharp/3modulo-Executar-formatacao-de-cadeia-de-caracteres-basica-CSharp/Interpolacao de cadeia de caracteres.cs
@@ -0,0 +1,7 @@
+/* 
+ Etapa 1 - Usar a interpolação de cadeia de caracteres para combinar uma cadeia de caracteres literal e um valor variável.
+*/
+
+string firstName = "Bob";
+string massage = $"Hello {firstName}!";
+Console.WriteLine(massage);
